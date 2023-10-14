598af9160b880cfd4ee5c57f6c03017e6d2f697d
LARBI Ishak <ishak.larbi@inttic.dz>
2023-10-14 20:54:39 +0000

    some modifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image'
 
-export default function Home() {
+async function consumeStarWars(){
+  // fetch on the server
+  // you can use caching, data revalidation ...
+  const res = await fetch("https://swapi.dev/api/starships/9", {next: {revalidate: 10}})
+  // alternatively you can use this 
+  // export const revalidate = 3600 // revalidate at most every hour but in the file scope
+  const data = res.json()
+  return data
+}
+
+export default async function Home() {
+  const data = await consumeStarWars()
+
   return (
     <main >
       just some thing
+      {JSON.stringify(data)}
     </main>
   )
 }
+
